d8a7ed3a30c16270bbd38a0014dca6158701d7e7
JeenaJamesk <jeenaanvin@gmail.com>
2025-08-17 21:40:34 +0000

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <html lang="en">
-            <body style={{ margin: 0}}>
-                <main>{children}</main>
-            </body>
-        </html>
-    );
+
 export const metadata: Metadata = {
   title: "Hotel Reservation",
   description: "Responsive Next.js app with Tailwind",
